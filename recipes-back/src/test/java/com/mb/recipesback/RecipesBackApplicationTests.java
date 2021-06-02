8d086473e72ed487e43f75873d57829a9aedc620
mbenazzouzaù <benazzouzamehdi@gmail.com>
2021-06-02 21:46:54 +0200

    changing folders hierarchy

---
+++ recipes-back/src/test/java/com/mb/recipesback/RecipesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mb.recipesback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
